ae1b558cceab83ed73e4c8080c7a2ce5423f4c78
cs1302 <mail@example.com>
2021-11-02 18:09:35 +0800

    Lab6 path fixed

--- Lab6/conf.py
+++ Lab6/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('~/cs1302/Lecture6'))
+sys.path.insert(0, os.path.expanduser('~/cs1302/Lecture6'))
 
 
 # -- Project information -----------------------------------------------------
